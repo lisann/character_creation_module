63855cc59ac9b7f691a083f4575c807a656a2173
asakhanevich <asakhanevich@ozon.ru>
2023-08-04 16:44:36 +0300

    Edit code style

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name, char_class):
+    """Функция атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +16,7 @@ def attack(char_name, char_class):
 
 
 def defence(char_name, char_class):
+    """Функция урона."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -23,6 +26,7 @@ def defence(char_name, char_class):
 
 
 def special(char_name, char_class):
+    """Функция специального умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +37,7 @@ def special(char_name, char_class):
 
 
 def start_training(char_name, char_class):
+    """Старт тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +62,7 @@ def start_training(char_name, char_class):
 
 
 def choice_char_class():
+    """Функция выбора персонажа"""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -78,7 +84,8 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
